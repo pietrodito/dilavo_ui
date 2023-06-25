279c63f21aebe700a4b5b262a9f09a179230eb71
Pierre Balaye <pierre@balaye.fr>
2023-06-25 06:13:09 +0000

    Add reset events

--- server.R
+++ server.R
@@ -1,10 +1,18 @@
 server <- function(input, output, session) {
 
- ## TODO deal with RESET button
+ reset_event <- function(CHAMP, STATUT) {
+  champ  <- str_to_lower(CHAMP)
+  statut <- str_to_lower(STATUT)
 
- observeEvent(input$reset_mco_dgf, {
-  f <- list.files("data", include.dirs = T, full.names = T, recursive = T)
-  file.remove(f)
-  session$reload()
- })
+  input_var <- str_c("reset", champ, statut, sep = "_")
+  data_path <- str_c("data/", CHAMP, "_", STATUT)
+
+  observeEvent(input[[input_var]], {
+   f <- list.files(data_path, include.dirs = T, full.names = T, recursive = T)
+   file.remove(f)
+   session$reload()
+  })
+ }
+
+ pwalk(items_loop, reset_event)
 }
