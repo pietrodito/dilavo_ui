97da74fd8b94f99f1f54b6f013b92dc192174ac1
Pierre Balaye <pierre@balaye.fr>
2023-06-26 09:30:05 +0000

    Refactor server: fix details'

--- server.R
+++ server.R
@@ -1,5 +1,12 @@
 server <- function(input, output, session) {
 
+js_black_header_callback <- paste(
+   "function(settings, json) {",
+    "$(this.api().table().header()).css(
+                         {'background-color': '#ccc', 'color': '#000'});",
+    "}")
+
+
  reset_event <- function(champ, statut) {
 
   input_var <- str_c("reset", champ, statut, sep = "_")
@@ -12,26 +19,38 @@ server <- function(input, output, session) {
   })
  }
 
-
- create_score_data_var <- function(champ, statut) {
-  eval(parse(text = str_c("score_data_", champ, "_", statut, "<<- NULL")))
+ score_path <- function(champ, statut) {
+  str_c("data/", champ, "_", statut, "/scores.csv")
  }
 
+ score_data <- list(NULL)
+
+ load_score_data <- function(champ, statut) {
+  path <- score_path(champ, statut)
+  suffixe <- str_c(champ, "_", statut)
+  if(file.exists(score_path(champ, statut))) {
+   print("file exists")
+   score_data[[suffixe]] <<- read_csv(path)
+  }
+ }
 
  display_score <- function(champ, statut) {
 
+  suffixe <- str_c(champ, "_", statut)
   output_var <- str_c("dash", champ, statut, sep = "_")
-  data_var   <- str_c("score_data", champ, statut, sep = "_")
 
-  output[[output_var]] <- if(! is.null(get(data_var))) {
-   renderDT(get(data_var),
+  data <- score_data[[suffixe]]
+
+  output[[output_var]] <- if(! is.null(data)) {
+   renderDT(data,
             rownames = FALSE,
-            selection = list(mode = "single", target = "cell"),
+            selection = list(mode = "single",
+                             target = "cell"),
             options = list(columnDefs = list(
-             list(className = 'dt-center',
-                  targets = 0:(ncol(score_data_mco_dgf) - 1))),
+            list(className = 'dt-center',
+                  targets = 0:(ncol(data) - 1))),
              dom = 't',
-             pageLength = nrow(get(data_var)),
+             pageLength = Inf,
              initComplete = JS(js_black_header_callback)))
   } else {
    renderDT(tibble(`Pour commencer...` =
@@ -41,8 +60,26 @@ server <- function(input, output, session) {
    }
   }
 
+ score_data_upload_fns <- list(NULL)
+
+ event_upload_score_data <- function(champ, statut) {
+
+  suffixe <- str_c(champ, "_", statut)
+  id <- str_c("MAJscores_", suffixe)
+  score_data_upload_fns[[suffixe]] <<- reactive({
+    req(input[[id]])
+    filestr <- input[[id]]
+    file <- read_csv2(filestr$datapath)
+    write_csv(file, score_path(champ, statut))
+    session$reload()
+   })
+
+  observeEvent((score_data_upload_fns[[suffixe]])(), {})
+ }
+
  server_logic <- list(reset_event,
-                      create_score_data_var,
+                      load_score_data,
+                      event_upload_score_data,
                       display_score)
 
  apply_server_logic <- function(champ, statut) {
@@ -50,4 +87,5 @@ server <- function(input, output, session) {
  }
 
  pwalk(items_loop, apply_server_logic)
+
 }
